dccc4775a76e11a9c53f2e37401bf94b643da030
KDY <doyoung0205@naver.com>
2020-11-10 23:27:24 +0900

    first commit

---
+++ src/main/java/me/doyoung/alley/app/AlleyApiServerApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.alley.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlleyApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlleyApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/doyoung/alley/app/AlleyApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.alley.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlleyApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
